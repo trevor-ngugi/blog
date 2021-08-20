5bd1b6e93546b8ec43aa04cf13ed81c0a1240edf
trevor ngugi <ngugitrevor@gmail.com>
2021-08-21 00:29:52 +0300

    add header

--- index.php
+++ index.php
@@ -20,31 +20,9 @@
     <!-- navbar -->
 
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">TNG </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="portfolio.html">Portfolio</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.php">Contact</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="index.php">Blog</a>
-                </li>
-                
-
-            </ul>
-        </div>
-    </nav>
+    <?php 
+    include_once'header.php';
+    ?>
 
     <div class="pic-1">
         <img src="images/landscape.jpg" alt="Girl in a jacket" width="100%" height="100%">
