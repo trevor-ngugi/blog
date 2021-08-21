ab7d4aee2ddd9ef8376a8ffe5f06ee6d2a5fc071
trevor ngugi <ngugitrevor@gmail.com>
2021-08-21 17:10:52 +0300

    added default homepage for the blog

---
+++ home.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'header.php';
+?>
+
+<div class="card" style="width: 18rem;">
+  <img src="images/blog.png" class="card-img-top" alt="blog pic">
+  <div class="card-body">
+    <h5 class="card-title">title</h5>
+    <p class="card-text">content:Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+  <div class="card-body">
+    <a href="#" class="card-link">like</a>
+    <a href="#" class="card-link">comment</a>
+  </div>
+  <ul class="list-group list-group-flush">
+    <li class="list-group-item">category:Cras justo odio</li>
+    <li class="list-group-item">peoplse comments</li>
+    <li class="list-group-item">people comments</li>
+  </ul>
+</div><br><br>
+
+
+
+
+
+<?php
+include_once 'footer.php';
+?>
