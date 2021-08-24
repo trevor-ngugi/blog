9b5617b46c08e81235d295959c4e11c3bd623bcb
trevor ngugi <ngugitrevor@gmail.com>
2021-08-25 00:04:11 +0300

    edit register form

--- register.php
+++ register.php
@@ -2,17 +2,21 @@
     <?php 
     include_once'header.php';
     ?>
-
+    <link rel="stylesheet" href="styles/register.css">
+    <div class="bg">
+    <img src="images/register.png" alt="pic">
     
     <div class="register">
+        
         <h2>register form</h2>
-        <form action="include/signup.inc.php" method="post">
-            <input type="text" name="name" placeholder="full name"><br><br>
-            <input type="text" name="email" placeholder="email"><br><br>
-            <input type="text" name="uid" placeholder="username"><br><br>
-            <input type="password" name="pwd" placeholder="password"><br><br>
-            <input type="password" name="pwdrepeat" placeholder="repeat password"><br><br>
-            <button type="submit" name="submit">sign up</button><br>
+        <!-- <img src="images/register.png" alt="pic"> -->
+        <form action="include/signup.inc.php" method="post" >
+            <input class="input" type="text" name="name" placeholder="full name"><br><br>
+            <input class="input" type="text" name="email" placeholder="email"><br><br>
+            <input class="input" type="text" name="uid" placeholder="username"><br><br>
+            <input class="input" type="password" name="pwd" placeholder="password"><br><br>
+            <input class="input" type="password" name="pwdrepeat" placeholder="repeat password"><br><br>
+            <button type="submit" class="btn"name="submit">sign up</button><br>
         </form>
         <?php 
     if(isset($_GET["error"])){
@@ -40,6 +44,7 @@
         }
     }
     ?>
+    </div>
     </div>
 
     
